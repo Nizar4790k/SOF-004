b7697fe75cfe0c26c855763104803a505f62e089
Nizar Sanchez <nizar4790k@gmail.com>
2019-09-20 21:10:26 -0400

    Views added to the GUI

--- Tarea4/app/src/main/java/com/example/tarea4/MainFragment.java
+++ Tarea4/app/src/main/java/com/example/tarea4/MainFragment.java
@@ -1,4 +1,26 @@
 package com.example.tarea4;
 
-public class MainFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return null;
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
